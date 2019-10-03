da0bee4551ea61c266bd464f3b4937343a8fe36d
Dmitry Nogin <dmitryn@mail.com>
2019-10-02 23:22:50 -0700

    Implement ambient context attribute

---
+++ Ambient.AspNetCore/AmbientContextAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace Ambient
+{
+    public class AmbientContextAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            using (new Op(context.HttpContext.Request.GetDisplayUrl()))
+            using (new Cancellation(context.HttpContext.RequestAborted))
+                await next();
+        }
+    }
+}
